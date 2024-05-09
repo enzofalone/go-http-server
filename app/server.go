cef5b586449a3db71c9c4c586eae40a9010e0997
Enzo Falone <enzofalone123@gmail.com>
2024-05-09 15:22:24 -0400

    pass stage

--- app/server.go
+++ app/server.go
@@ -2,22 +2,43 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net"
 	"os"
 )
 
+func handleConnection(conn net.Conn) {
+	// free up memory by closing connection once satisfied
+	defer conn.Close()
+
+	response := "HTTP/1.1 200 OK\r\n\r\n"
+
+	if _, err := conn.Write([]byte(response)); err != nil {
+		log.Fatal(err)
+	}
+
+}
+
 func main() {
 	fmt.Println("Logs from your program will appear here!")
 
-	l, err := net.Listen("tcp", "0.0.0.0:4221")
+	l, err := net.Listen("tcp", ":4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
-	
-	_, err = l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	defer l.Close()
+
+	for {
+		// wait for any new connections
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+
+		// handle new connection by passing all information
+		go handleConnection(conn)
 	}
+
 }
